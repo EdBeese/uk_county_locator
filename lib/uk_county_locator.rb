842fb7cf11a574b03e0bc027e2588f1c51138a64
Edward Beesley <ed@homeflow.co.uk>
2025-01-10 15:57:27 +0000

    Initial Push
    Actions:
    
    + Pushing initial boiler plate gem
    
    Motivation:
    
    +

---
+++ lib/uk_county_locator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "uk_county_locator/version"
+
+module UkCountyLocator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/uk_county_locator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe UkCountyLocator do
+  it "has a version number" do
+    expect(UkCountyLocator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
