a6c04a98b9228d966f4359983817fcfde430d51f
Edward Beesley <ed@homeflow.co.uk>
2025-01-21 09:30:42 +0000

    Add Initial Lookup Functionality
    Actions:
    
    + Added files that contain hashes of encoded polygon
    data for the three types of county.
    
    + Added uk_county_locator wrapped module that kicks
    off logic.
    
    + Added a locator class that encapsulates the logic
    to find a polygon from passed in coordinates.
    
    + Added geokit, parallel and polylines gem dependencies.
    
    + Added polylines_service class that encapsulates
    all of the logic to decode polygon strings and
    determine whether a point falls within them.
    
    + Added specs, fixtures to test each polygon and
    a shared example to iterate through a fixture for
    a county type.
    
    + Set logic, so that .find_county will return a
    ceremonial county by default, but the user can
    pass in a type to return either historic or
    current, or all three in a hash.
    
    Motivation:
    
    + Found that Geokit gem was the fastest to decode
    the polylines.
    
    + Using parallel to work through the polygons in
    parallel and return a result faster.
    
    + Quicker to return a single county, and, after
    consideration, thought it most likely that a user
    would want to fetch a Ceremonial County.

--- spec/uk_county_locator_spec.rb
+++ spec/uk_county_locator_spec.rb
@@ -1,11 +1,73 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
+require 'yaml'
+
 RSpec.describe UkCountyLocator do
-  it "has a version number" do
+  it 'has a version number' do
     expect(UkCountyLocator::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '.find_county' do
+    let(:latitude) { 51.4290383462936 }
+    let(:longitude) { -0.1658028043302774 }
+
+    context 'when no county type argument is passed in' do
+      it 'returns the Ceremonial County by default' do
+        expect(UkCountyLocator.find_county(latitude, longitude)).to eq('Greater London')
+      end
+    end
+
+    context 'when historic county type argument is passed in' do
+      it 'returns the Historic County' do
+        expect(UkCountyLocator.find_county(latitude, longitude, type: :historic)).to eq('Surrey')
+      end
+    end
+
+    context 'when current county/ unitary authority type argument is passed in' do
+      it 'returns the Historic County' do
+        expect(UkCountyLocator.find_county(latitude, longitude, type: :current)).to eq('Wandsworth')
+      end
+    end
+
+    context 'when all county type argument is passed in' do
+      let(:expected_result) do
+        {
+          current_county_or_unitary_authority: 'Wandsworth',
+          ceremonial_county: 'Greater London',
+          historic_county: 'Surrey'
+        }
+      end
+
+      it 'returns a hash with all three county types' do
+        expect(UkCountyLocator.find_county(latitude, longitude, type: :all)).to eq(expected_result)
+      end
+    end
+
+    describe 'Ceremonial County Locator' do
+      let(:coordinates) do
+        YAML.load_file(File.join(__dir__, 'fixtures/ceremonial_county_test_coordinates.yml'))['test_coordinates']
+      end
+
+      include_examples 'county type matcher', :ceremonial
+    end
+
+    describe 'Historic County Locator' do
+      let(:coordinates) do
+        YAML.load_file(File.join(__dir__, 'fixtures/historic_county_test_coordinates.yml'))['test_coordinates']
+      end
+
+      include_examples 'county type matcher', :historic
+    end
+
+    describe 'Current County and Unitary Authority Locator' do
+      let(:coordinates) do
+        YAML.load_file(
+          File.join(__dir__, 'fixtures/current_county_and_unitary_authority_test_coordinates.yml')
+        )['test_coordinates']
+      end
+
+      include_examples 'county type matcher', :current
+    end
   end
 end
